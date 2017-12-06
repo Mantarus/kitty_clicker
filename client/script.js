1a02f994f0c773d837f70153ffb63bff905423fe
Mantarus <m.mantarus@gmail.com>
2017-12-07 03:17:16 +0400

    Improve clicker

--- client/script.js
+++ client/script.js
@@ -1,3 +1,37 @@
+let clicks = 0;
+let autoclickersCount = 0;
+let autoclickersPower = 2;
+let lastSavedClicks = 0;
+
+setInterval(updateClicksPerSecond, 1000);
+
 function increase() {
-    count.textContent = parseInt(count.textContent, 10) + 1;
+    clicks += 1;
+    updateClicksCount();
+}
+
+function addAutoClicker() {
+    autoclickersCount += 1;
+    document.getElementById("autoclickerCount").textContent = autoclickersCount;
+    setInterval(autoclick, 1000);
+}
+
+function upgradeAutoClicker() {
+    autoclickersPower += 2;
+    document.getElementById("autoclickerPower").textContent = autoclickersPower;
+}
+
+function autoclick() {
+    clicks += autoclickersPower;
+    updateClicksCount();
+}
+
+function updateClicksCount() {
+    document.getElementById("count").textContent = clicks;
+}
+
+function updateClicksPerSecond() {
+    let clicksPerSecond = clicks - lastSavedClicks;
+    lastSavedClicks = clicks;
+    document.getElementById("cps").textContent = clicksPerSecond;
 }
